d423979bc75ae090d2fa57b01eed68b49ff68c78
kellyhosokawa <kellykhosokawa@gmail.com>
2015-08-10 23:31:32 -0700

    Updated settings to show the user's selection/input

--- FoodApp/app/src/main/java/com/khmkau/codeu/foodapp/MainActivity.java
+++ FoodApp/app/src/main/java/com/khmkau/codeu/foodapp/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
-            startActivity(new Intent(this, PreferencesActivity.class));
+            startActivity(new Intent(this, Preferences.class));
             return true;
         }
         return super.onOptionsItemSelected(item);
